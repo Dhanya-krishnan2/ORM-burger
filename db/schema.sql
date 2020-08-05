c7e89c69a4b0ad0b88de750b670d77893bf28808
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-08-06 01:01:43 +0800

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+-- Table Creation
+CREATE TABLE burgers(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(50),
+devoured BOOLEAN,
+date TIMESTAMP);
