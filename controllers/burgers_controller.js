9735d639776651d936607ddb6073891bae13ca73
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2020-08-06 23:45:00 +0800

    Changed burger_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -27,7 +27,7 @@ router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
-
+   // we will the correct error code by looping through id
     burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404.
