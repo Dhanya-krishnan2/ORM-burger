0a09cc7860ca73de816f05e36eada9ba611086a6
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2020-08-06 23:02:26 +0800

    Added some meaningful comments

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
-
+// here adding jaws db as a add on to enable deployment in heroku 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -11,7 +11,8 @@ if (process.env.JAWSDB_URL) {
         database: "burgers_db"
     });
 };
-
+// for the local connection I am using my database details above.
+// the connection is establishing from here and if its connect show the connected message with the id otherwise throw error
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
