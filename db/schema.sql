339b565fe7f840238209c89e8ab1d740062a9823
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2020-08-06 23:46:30 +0800

    updated the timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,4 @@ id INTEGER AUTO_INCREMENT PRIMARY KEY,
 burger_name VARCHAR(50),
 devoured BOOLEAN,
 date TIMESTAMP);
+-- this are the queries we used to create a database and make sure we add the date, and time as well
