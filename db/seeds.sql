dc17c51cd404143120b3b068c6437c739a9d71b6
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2020-08-10 18:21:23 +0800

    updated some values in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,10 @@ VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
   ("Quarter Pounder", false, "2016-11-05 16:20:01"),
   ("McRoyal", false, "2016-11-05 04:20:20"),
   ("Double Double", true, "2016-11-05 16:20:16");
+  ("Big Mac", false, "2020-11-05 04:20:00"),
+  ("Whooper", false, "2020-11-05 04:20:01"),
+  ("Cheezburger", false, "2020-11-05 04:20:02"),
+  ("Baconator", false, "2020-05-05 16:20:00"),
+  ("Quarter Pounder", false, "2020-03-05 16:20:01"),
+  ("McRoyal", false, "2020-02-05 04:20:20"),
+  ("Double Double", true, "2019-11-05 16:20:16");
